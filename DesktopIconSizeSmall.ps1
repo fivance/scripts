c6edeb0f44c04083a6bf50e6a77f9853a5bc2e28
fivance <fran.ivancevic7@gmail.com>
2025-04-21 21:17:33 +0200

    Update DesktopIconSizeSmall.ps1

--- DesktopIconSizeSmall.ps1
+++ DesktopIconSizeSmall.ps1
@@ -1,15 +1,11 @@
-# Change desktop icon size to small (value: 32 for small icons)
 $desktopIconSizeKey = "HKCU:\Software\Microsoft\Windows\Shell\Bags\1\Desktop"
 $iconSizeValue = 32
 
-# Create the registry key path if it doesn't exist
 If (-not (Test-Path $desktopIconSizeKey)) {
     New-Item -Path $desktopIconSizeKey -Force
 }
 
-# Set the Icon Size for desktop
 Set-ItemProperty -Path $desktopIconSizeKey -Name IconSize -Value $iconSizeValue
 
-# Restart explorer.exe to apply changes
 Stop-Process -Name explorer -Force
 Start-Process explorer
